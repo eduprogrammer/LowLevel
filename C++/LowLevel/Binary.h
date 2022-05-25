08dbf7b8256eb0c827661784b756b2fffe115ef4
Eduardo Programador <consultoria@eduardoprogramador.com>
2022-05-25 00:15:53 -0300

    improved C++ perfomance

--- C++/LowLevel/Binary.h
+++ C++/LowLevel/Binary.h
@@ -1,1 +1,141 @@
-//code...
+#include "Number.h"
+#include "LowLevelException.h"
+#include "LowLevel.h"
+#include <cmath>
+
+class Binary : public Number
+{
+public:
+	Binary() : Number() {};
+	Binary(const char* number) : Number(number) {};
+	Binary(const char* first, const char* second) : Number(first, second) {};
+	~Binary() {};
+
+	char* toDecimal()
+	{
+		if (this->number == nullptr)
+		{
+			throw new LowLevelException("You must to load the Binary Class with the construct that has a valid binary inside.");
+		}
+		else
+		{			
+
+			double res = 0;
+			double calc = 0;
+			int times = 0;
+			for (int i = strlen(this->number) - 1; i >= 0; i--)
+			{
+				char n = this->number[i];				
+				calc = (n == '0') ? 0 : pow(2, times);
+				times++;
+				res += calc;
+			}			
+									
+			static char temp[50] = {0};			
+			sprintf(temp, "%f", res);			
+			static char *p = strtok(temp, ".");						
+			return p;
+		}
+	}
+	char* toDecimal(const char* number)
+	{
+		double res = 0;
+		double calc = 0;
+		int times = 0;
+		for (int i = strlen(number) - 1; i >= 0; i--)
+		{
+			char n = number[i];
+			calc = (n == '0') ? 0 : pow(2, times);
+			times++;
+			res += calc;
+		}
+
+		static char temp[50] = { 0 };
+		sprintf(temp, "%f", res);
+		static char* p = strtok(temp, ".");
+		return p;
+	}
+
+	char* add()
+	{
+		//code...
+
+		char* code = new char[5];
+		return code;
+	}
+	char* add(const char* numOne, const char* numTwo)
+	{
+		char* code = new char[5];
+		return code;
+	}
+	int smallest()
+	{
+		return 0;
+	}
+	int smallest(const char* numOne, const char* numTwo)
+	{
+		return 0;
+	}
+	char* cutZeros()
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* cutZeros(const char* number)
+	{
+		char* code = new char[5];
+		return code;
+	}
+
+	char* fillWithZeros(int count)
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* fillWithZeros(int count, const char* number)
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* subtract()
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* subtract(const char* numOne, const char* numTwo)
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* multiply()
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* multiply(const char* num, const char* by)
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* divide()
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* divide(const char* numOne, const char* by)
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* modulus()
+	{
+		char* code = new char[5];
+		return code;
+	}
+	char* modulus(const char* numOne, const char* by)
+	{
+		char* code = new char[5];
+		return code;
+	}
+
+};
--- C++/LowLevel/Number.h
+++ C++/LowLevel/Number.h
@@ -1,1 +1,63 @@
-//code...
+#include <cstdlib>
+#include <cstring>
+
+#pragma warning(disable: 4996)
+
+class Number {
+protected:
+	char* number;
+	char* first, * second;
+
+	Number()
+	{
+		this->first = nullptr;
+		this->second = nullptr;
+		this->number = nullptr;
+	};
+	Number(const char* number)
+	{
+		this->number = new char[strlen(number)];
+		strcpy(this->number, number);
+		this->first = NULL;
+		this->second = nullptr;
+	}
+	Number(const char* first, const char* second)
+	{
+		this->first = new char[strlen(first)];
+		this->second = new char[strlen(second)];
+
+		strcpy(this->first, first);
+		strcpy(this->second, second);
+		this->number = nullptr;
+	}
+
+	virtual char* toDecimal() = 0;
+	virtual char* toDecimal(const char* number) = 0;
+	virtual char* add() = 0;
+	virtual char* add(const char* numOne, const char* numTwo) = 0;
+	virtual int smallest() = 0;
+	virtual int smallest(const char* numOne, const char* numTwo) = 0;
+	virtual char* cutZeros() = 0;
+	virtual char* cutZeros(const char* number) = 0;
+	virtual char* fillWithZeros(int count) = 0;
+	virtual char* fillWithZeros(int count, const char* number) = 0;
+	virtual char* subtract() = 0;
+	virtual char* subtract(const char* numOne, const char* numTwo) = 0;
+	virtual char* multiply() = 0;
+	virtual char* multiply(const char* num, const char* by) = 0;
+	virtual char* divide() = 0;
+	virtual char* divide(const char* numOne, const char* by) = 0;
+	virtual char* modulus() = 0;
+	virtual char* modulus(const char* numOne, const char* by) = 0;
+
+
+
+public:
+	static const char** listNumberTypes()
+	{
+		const char* res[] = {"Binary","Hexadecimal","Octal"};
+		return res;
+
+	}
+
+};
